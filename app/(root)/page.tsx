25456d0f1387b9abe1056a36d01ef1fd6c4fa22f
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-07-23 10:40:20 +0300

    test

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
     return (
       <main>
-        <Button variant="outline">ok</Button>test
+        <Button variant="outline">ok</Button>
       </main>
     )
   }
